52bf261707cf4bd155ead86504a2e46015159ff9
StygianLightning <denrellum@gmail.com>
2023-12-08 20:26:16 +0100

    Compute paths.
    gdc and least common multiple.
    
    Cheat a little using the properties of the input data to simplify the code.

--- src/bin/8.rs
+++ src/bin/8.rs
@@ -1,5 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
+use gcd::Gcd;
+
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Direction {
     Left,
@@ -29,7 +31,6 @@ struct Node {
 #[derive(Debug, Default)]
 struct Network {
     nodes: Vec<Node>,
-
     name_to_id: HashMap<String, usize>,
 }
 
@@ -68,6 +69,31 @@ impl Network {
             Direction::Right => node.right,
         }
     }
+
+    fn node_name(&self, node: usize) -> &str {
+        &self.nodes[node].name
+    }
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+struct IterationNode {
+    node: usize,
+    relative_iteration: usize,
+}
+
+#[derive(Debug, Default)]
+struct NodeHistory {
+    iteration_nodes: Vec<IterationNode>,
+    loop_info: LoopInfo,
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+struct LoopInfo {
+    start: usize,
+    length: usize,
+}
+fn least_common_multiple(x: u128, y: u128) -> u128 {
+    x * y / x.gcd(y)
 }
 
 fn main() {
@@ -106,7 +132,7 @@ fn main() {
         "ZZZ"
     };
 
-    let mut start_nodes = network
+    let start_nodes = network
         .name_to_id
         .iter()
         .filter(|(k, _v)| k.ends_with(start_node_filter))
@@ -134,8 +160,11 @@ fn main() {
             relative_iteration: 0,
         };
         visited_relative_nodes.insert(last_seen_node);
+        node_history.iteration_nodes.push(last_seen_node);
 
         for (iteration, direction) in directions.iter().cycle().cloned().enumerate() {
+            let previous_node = current_node % directions.len();
+            let previous_iteration = iteration;
             current_node = network.node_neighbor(current_node, direction);
             let iteration = (iteration + 1) % directions.len();
 
@@ -173,23 +202,87 @@ fn main() {
             "loop info for node {node_name}: {:#?}",
             node_history.loop_info
         );
+
+        let loop_len_mod_direction_len = node_history.loop_info.length % directions.len();
+        let loop_len_div_direction_len = node_history.loop_info.length / directions.len();
+
+        println!("loop length % direction length: {loop_len_mod_direction_len}");
+        println!("loop length / direction length: {loop_len_div_direction_len}");
     }
+
+    let target_nodes_per_start_node = history_per_node
+        .iter()
+        .map(|history| {
+            history
+                .iteration_nodes
+                .iter()
+                .enumerate()
+                .filter_map(|(i, n)| target_nodes.contains(&n.node).then_some(i))
+                .collect::<HashSet<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    for (idx, target_node_ids) in target_nodes_per_start_node.iter().enumerate() {
+        println!("target_nodes for start node index {idx}: {target_node_ids:?}");
+        println!(
+            "first target_node for start node index {idx} % directions.len(): {:} / directions.len(): {}",
+            target_node_ids.iter().next().unwrap() % directions.len(),
+            target_node_ids.iter().next().unwrap() / directions.len()
+        );
+    }
+
+    // Turns out there's actually only one target index in each node history, and that target is always on the loop in our input files.
+    // Note that in general, it's possible that there are multiple target indices that need to be taken into account
+    let target_indices = target_nodes_per_start_node
+        .iter()
+        .map(|coll| coll.iter().next().unwrap())
+        .cloned()
+        .collect::<Vec<_>>();
+
+    // start index: largest individual index. These are actually all on multiples of directions.len()
+    let largest_target_idx = *target_indices.iter().max().unwrap();
+    println!("largest target idx: {}", largest_target_idx);
+
+    //now need least common multiple of loop lengths to make sure all end up on their target at the same time
+    let loop_lengths_direction_multiples = history_per_node
+        .iter()
+        .map(|h| (h.loop_info.length / directions.len()) as u128)
+        .collect::<Vec<_>>();
+
+    println!(
+        "loop lengths as multiples of directions.len(): {:?}",
+        loop_lengths_direction_multiples
+    );
+
+    let least_common_multiple_loop_len = loop_lengths_direction_multiples
+        .iter()
+        .cloned()
+        .reduce(least_common_multiple)
+        .unwrap();
+
+    println!("least common multiple loop len: {least_common_multiple_loop_len}");
+
+    // Normally, this wouldn't be enough -- the nodes could be offset and require more iterations.
+    // However, all target nodes in the input files are at the end of the loops, so we can cheat a little:
+    let total = least_common_multiple_loop_len * directions.len() as u128;
+    println!("total: {total}");
 }
 
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
-struct IterationNode {
-    node: usize,
-    relative_iteration: usize,
-}
+#[cfg(test)]
+mod test {
+    use super::*;
+    use gcd::Gcd;
 
-#[derive(Debug, Default)]
-struct NodeHistory {
-    iteration_nodes: Vec<IterationNode>,
-    loop_info: LoopInfo,
-}
+    #[test]
+    fn test_gdc() {
+        assert_eq!(2u32.gcd(4), 2);
+        assert_eq!(2u32.gcd(5), 1);
+        assert_eq!(13u32.gcd(5), 1);
+        assert_eq!(32u32.gcd(12), 4);
+    }
 
-#[derive(Debug, Clone, Copy, Default)]
-struct LoopInfo {
-    start: usize,
-    length: usize,
+    #[test]
+    fn test_least_common_multiple() {
+        assert_eq!(least_common_multiple(13, 8), 13 * 8);
+    }
 }
