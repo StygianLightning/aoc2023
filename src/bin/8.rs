9f85ddf89d66cc4517dfdc899192261ec6eb8911
StygianLightning <denrellum@gmail.com>
2023-12-08 14:48:14 +0100

    Added network structure and parsing.

--- src/bin/8.rs
+++ src/bin/8.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Direction {
     Left,
@@ -16,8 +18,69 @@ impl TryFrom<char> for Direction {
     }
 }
 
+#[derive(Debug)]
+struct Node {
+    index: usize,
+    left: usize,
+    right: usize,
+}
+
+#[derive(Debug, Default)]
+struct Network {
+    nodes: Vec<Node>,
+
+    name_to_id: HashMap<String, usize>,
+
+    start_index: usize,
+    target_index: usize,
+}
+
+impl Network {
+    fn get_or_insert(&mut self, name: &str) -> usize {
+        let id = *self
+            .name_to_id
+            .entry(name.to_owned())
+            .or_insert(self.nodes.len());
+
+        if id >= self.nodes.len() {
+            self.nodes.push(Node {
+                index: id,
+                left: 0,
+                right: 0,
+            })
+        }
+
+        id
+    }
+
+    fn insert_edges(&mut self, name: &str, left: &str, right: &str) {
+        let start = self.get_or_insert(name);
+        let left = self.get_or_insert(left);
+        let right = self.get_or_insert(right);
+
+        self.nodes[start].left = left;
+        self.nodes[start].right = right;
+    }
+
+    fn node_neighbor(&self, node: usize, direction: Direction) -> usize {
+        let node = &self.nodes[node];
+        match direction {
+            Direction::Left => node.left,
+            Direction::Right => node.right,
+        }
+    }
+
+    fn set_start(&mut self, start: usize) {
+        self.start_index = start;
+    }
+
+    fn set_target(&mut self, target: usize) {
+        self.target_index = target;
+    }
+}
+
 fn main() {
-    let input = std::fs::read_to_string("input/8_training.txt").unwrap();
+    let input = std::fs::read_to_string("input/8_training2.txt").unwrap();
 
     let mut lines = input.lines();
     let directions: Vec<Direction> = lines
@@ -27,5 +90,20 @@ fn main() {
         .map(|c| Direction::try_from(c).unwrap())
         .collect();
 
-    println!("{directions:?}");
+    let mut network = Network::default();
+    for line in lines.skip(1) {
+        let mut node_and_neighbors = line.split('=');
+        let node = node_and_neighbors.next().unwrap().trim();
+        let rest = node_and_neighbors.next().unwrap().trim();
+        let mut left_right = rest.split('(').nth(1).unwrap().split(',');
+        let left = left_right.next().unwrap().trim();
+        let right = left_right.next().unwrap().split(')').next().unwrap().trim();
+
+        network.insert_edges(node, left, right);
+    }
+
+    let start = network.get_or_insert("AAA");
+    network.set_start(start);
+    let target = network.get_or_insert("ZZZ");
+    network.set_target(target);
 }
