0a65569280d115e2cf1601f4ab93b944c5b9b2d6
StygianLightning <denrellum@gmail.com>
2023-12-10 13:04:13 +0100

    Day 10 logic part 1: Find loops.
    Adjusted position is_valid check: y index corresponds to height, so it's
    the first index used to access a nested array.

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,4 +1,5 @@
 use core::panic;
+use std::ops::Add;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Tile {
@@ -21,9 +22,9 @@ struct Position {
 impl Position {
     fn to_index<T>(self, grid: &Vec<Vec<T>>) -> Option<(usize, usize)> {
         if self.x < 0
-            || self.x as usize >= grid.len()
+            || self.x as usize >= grid[0].len()
             || self.y < 0
-            || self.y as usize >= grid[0].len()
+            || self.y as usize >= grid.len()
         {
             None
         } else {
@@ -44,22 +45,32 @@ impl Offset {
     }
 }
 
+impl Add<Offset> for Position {
+    type Output = Position;
+
+    fn add(self, rhs: Offset) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
 const UP: Offset = Offset { x: 0, y: -1 };
 const DOWN: Offset = Offset { x: 0, y: 1 };
 const LEFT: Offset = Offset { x: -1, y: 0 };
 const RIGHT: Offset = Offset { x: 1, y: 0 };
 
 impl Tile {
-    fn neighbors(self) -> (Offset, Offset) {
+    fn neighbors(self) -> [Offset; 2] {
         match self {
-            Tile::UpDown => (UP, DOWN),
-            Tile::LeftRight => (LEFT, RIGHT),
-            Tile::UpRight => (UP, RIGHT),
-            Tile::UpLeft => (UP, LEFT),
-            Tile::DownRight => (DOWN, RIGHT),
-            Tile::DownLeft => (DOWN, LEFT),
-            Tile::Ground => (Offset::default(), Offset::default()),
-            Tile::Start => (Offset::default(), Offset::default()),
+            Tile::UpDown => [UP, DOWN],
+            Tile::LeftRight => [LEFT, RIGHT],
+            Tile::UpRight => [UP, RIGHT],
+            Tile::UpLeft => [UP, LEFT],
+            Tile::DownRight => [DOWN, RIGHT],
+            Tile::DownLeft => [DOWN, LEFT],
+            Tile::Ground => [Offset::default(), Offset::default()],
+            Tile::Start => [Offset::default(), Offset::default()],
         }
     }
 }
@@ -84,4 +95,85 @@ fn main() {
         .lines()
         .map(|line| line.chars().map(char_to_tile).collect::<Vec<_>>())
         .collect::<Vec<_>>();
+
+    println!("{grid:#?}");
+
+    let mut start = Position { x: -1, y: -1 };
+    for (i, row) in grid.iter().enumerate() {
+        for (j, val) in row.iter().enumerate() {
+            if *val == Tile::Start {
+                start = Position {
+                    x: j as i32,
+                    y: i as i32,
+                };
+            }
+        }
+    }
+
+    println!("start position: {start:?}");
+
+    let start_neighbors = [start + UP, start + DOWN, start + RIGHT, start + LEFT];
+
+    for potential_loop_start in start_neighbors {
+        if potential_loop_start.to_index(&grid).is_some() {
+            if let Some(found_loop) = find_loop(&grid, potential_loop_start, start, start) {
+                println!("found loop for start position {potential_loop_start:?}: {found_loop:?}");
+                println!("max distance: {}", found_loop.len / 2);
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Loop {
+    len: usize,
+}
+
+fn find_loop(
+    grid: &Vec<Vec<Tile>>,
+    mut current_position: Position,
+    mut previous_position: Position,
+    goal: Position,
+) -> Option<Loop> {
+    let mut len = 1;
+    while current_position != goal {
+        let index = current_position.to_index(grid).unwrap();
+        let tile = grid[index.1][index.0];
+
+        let mut next_position = Position { x: -1, y: -1 };
+        let mut connected = false;
+        for offset in tile.neighbors() {
+            if !offset.is_valid() {
+                // e.g. ground tile -> no loop
+                break;
+            }
+
+            let neighbor_pos = current_position + offset;
+
+            if neighbor_pos.to_index(grid).is_none() {
+                // off grid -> no loop
+                break;
+            }
+
+            if neighbor_pos == previous_position {
+                connected = true;
+            } else {
+                next_position = neighbor_pos;
+            }
+        }
+
+        if connected {
+            previous_position = current_position;
+            current_position = next_position;
+            len += 1;
+
+            if current_position == goal {
+                return Some(Loop { len });
+            }
+        } else {
+            // no connection to previous tile
+            break;
+        }
+    }
+    None
 }
