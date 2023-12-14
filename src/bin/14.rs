559d764f2e4098d5da83f181b13f3a7f577fab4b
StygianLightning <denrellum@gmail.com>
2023-12-14 16:04:35 +0100

    Day 14 part 1 logic.

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -20,7 +20,13 @@ impl Tile {
 }
 
 fn main() {
-    let input = std::fs::read_to_string("input/14_training.txt").unwrap();
+    let input_file = if let Some(file) = std::env::args().nth(1) {
+        file
+    } else {
+        "input/14_training.txt".to_owned()
+    };
+    println!("using input file `{input_file}`");
+    let input = std::fs::read_to_string(input_file).unwrap();
     println!("{input}");
     let num_lines = input.lines().count();
     let line_length = input.lines().next().unwrap().len();
@@ -48,4 +54,29 @@ fn main() {
 
         println!();
     }
+
+    let mut total = 0;
+    for x in 0..grid.len_x() as i32 {
+        let mut load_next = grid.len_y() as i32; // the examples are 1-indexed
+        let mut load_this_column = 0;
+        for y in 0..grid.len_y() as i32 {
+            //
+            let idx = Index2d { x, y };
+            match grid[idx] {
+                Tile::Space => continue,
+                Tile::Loose => {
+                    load_this_column += load_next;
+                    load_next -= 1;
+                }
+                Tile::Fixed => {
+                    load_next = grid.len_y() as i32 - y - 1;
+                }
+            }
+        }
+
+        println!("load column {x}: {load_this_column}");
+        total += load_this_column;
+    }
+
+    println!("total load: {total}");
 }
