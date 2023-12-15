93dda6757a59469a7a130efa7badb0c67f28a7a4
StygianLightning <denrellum@gmail.com>
2023-12-15 15:26:54 +0100

    Day 15 part 2 logic.

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -10,6 +10,35 @@ fn hash(s: &str) -> u32 {
     result
 }
 
+const NUM_BOXES: usize = 256;
+
+#[derive(Debug)]
+struct Lense {
+    focal_length: u32,
+    label: String,
+}
+
+#[derive(Debug, Default)]
+struct LenseBox {
+    lenses: Vec<Lense>,
+}
+
+impl LenseBox {
+    fn add(&mut self, lense: Lense) {
+        if let Some(pos) = self.lenses.iter().position(|l| l.label == lense.label) {
+            self.lenses[pos] = lense;
+        } else {
+            self.lenses.push(lense);
+        }
+    }
+
+    fn remove(&mut self, label: &str) {
+        if let Some(pos) = self.lenses.iter().position(|l| l.label == label) {
+            self.lenses.remove(pos);
+        }
+    }
+}
+
 fn main() {
     let input_file = if let Some(file) = std::env::args().nth(1) {
         file
@@ -29,9 +58,50 @@ fn main() {
 
     println!("part2 flag: {part2}");
 
-    let mut total = 0;
+    if !part2 {
+        let mut total = 0;
+        for s in input.split(',') {
+            total += hash(s);
+        }
+
+        println!("total: {total}");
+        return;
+    }
+
+    // part 2 logic
+    let mut boxes = (0..NUM_BOXES)
+        .map(|_| LenseBox::default())
+        .collect::<Vec<_>>();
+
     for s in input.split(',') {
-        total += hash(s);
+        if s.contains('=') {
+            let mut it = s.split('=');
+            let label = it.next().unwrap();
+            let focal_length = it.next().unwrap().parse::<u32>().unwrap();
+            let index = hash(label);
+            let lense_box = &mut boxes[index as usize];
+            lense_box.add(Lense {
+                focal_length,
+                label: label.to_owned(),
+            })
+        } else {
+            // remove
+            let label = s.split('-').next().unwrap();
+            let index = hash(label);
+            let lense_box = &mut boxes[index as usize];
+            lense_box.remove(label);
+        }
+    }
+
+    let mut total = 0;
+    for (i, b) in boxes.iter().enumerate() {
+        let mut lense_power = 0;
+        for (lense_idx, lense) in b.lenses.iter().enumerate() {
+            let lense_index_multiplier = lense_idx + 1;
+            lense_power += lense_index_multiplier as u32 * lense.focal_length;
+        }
+        let index_multiplier = i as u32 + 1;
+        total += lense_power * index_multiplier;
     }
 
     println!("total: {total}");
