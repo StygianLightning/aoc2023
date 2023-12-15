c21b1c848f7b758a270a4dd85b12386cf734e1f7
StygianLightning <denrellum@gmail.com>
2023-12-15 15:05:08 +0100

    Day 15 part 1 logic.

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,3 +1,15 @@
+fn hash(s: &str) -> u32 {
+    let mut result = 0;
+
+    for c in s.chars() {
+        result += c as u32;
+        result *= 17;
+        result %= 256;
+    }
+
+    result
+}
+
 fn main() {
     let input_file = if let Some(file) = std::env::args().nth(1) {
         file
@@ -5,7 +17,8 @@ fn main() {
         "input/15_training.txt".to_owned()
     };
     println!("using input file `{input_file}`");
-    let input = std::fs::read_to_string(input_file).unwrap();
+    let mut input = std::fs::read_to_string(input_file).unwrap();
+    input.retain(|c| c != '\n');
     println!("{input}");
 
     let part2 = if let Some(flag) = std::env::args().nth(2) {
@@ -15,4 +28,11 @@ fn main() {
     };
 
     println!("part2 flag: {part2}");
+
+    let mut total = 0;
+    for s in input.split(',') {
+        total += hash(s);
+    }
+
+    println!("total: {total}");
 }
