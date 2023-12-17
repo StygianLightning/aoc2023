560e4272ee46cb99ddb6bc90c0a8634f4b4b4385
StygianLightning <denrellum@gmail.com>
2023-12-17 15:06:57 +0100

    Read grid.

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,3 +1,5 @@
+use aoc2023::util::{Grid2d, Index2d};
+
 fn main() {
     let input_file = if let Some(file) = std::env::args().nth(1) {
         file
@@ -13,4 +15,23 @@ fn main() {
     } else {
         false
     };
+
+    let mut grid = Grid2d::new(input.lines().next().unwrap().len(), input.lines().count());
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, n) in line.char_indices() {
+            let num = n.to_string().parse::<u32>().unwrap();
+            grid[Index2d {
+                x: x as _,
+                y: y as _,
+            }] = num;
+        }
+    }
+
+    for y in 0..grid.len_y() as i32 {
+        for x in 0..grid.len_x() as i32 {
+            print!("{}", grid[Index2d { x, y }]);
+        }
+        println!();
+    }
 }
